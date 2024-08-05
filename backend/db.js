d5020681c9812a47b2c9d5026c01b32a81320b29
hepzishanthijohn <yyepsipal@gmail.com>
2024-08-05 21:43:12 +0530

    first commit

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 //connect to MongoDB
 const connectDB=async()=>{
-    await mongoose.connect(process.env.DB_HOST, {
+    await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
